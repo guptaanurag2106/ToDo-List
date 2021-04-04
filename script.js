9c1df683f0548602c4e229879d0236f1727a924a
ANURAG GUPTA <75490384+guptaanurag2106@users.noreply.github.com>
2021-04-04 23:39:42 +0530

    added localStorage and icons

--- script.js
+++ script.js
@@ -11,8 +11,11 @@ textbox.addEventListener("keyup", function (event) {
     }
 });
 
+window.onload = getTodos();
 function additem(e) {
     e.preventDefault();
+
+    saveLocalTodos(textbox.value);
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("div");
 
@@ -22,11 +25,11 @@ function additem(e) {
 
     const complete_btn = document.createElement("button");
     complete_btn.classList.add("complete_btn");
-    complete_btn.innerHTML = "completed";
+    complete_btn.innerHTML = '<i class="fa fa-check"></i>';
 
     const del_btn = document.createElement("button");
     del_btn.classList.add("del_btn");
-    del_btn.innerHTML = "delete";
+    del_btn.innerHTML = '<i class="fa fa-trash"></i>';
 
     if (textbox.value != "") {
         todoDiv.appendChild(item)
@@ -40,8 +43,68 @@ function additem(e) {
 
 function compDel(e) {
     const list_item = e.target;
-    if (list_item.classList[0] == "del_btn")
+    if (list_item.classList[0] == "del_btn") {
         list_item.parentElement.remove();
+        removeTodo(list_item.parentElement.children[0].innerText)
+    }
+
     else if (list_item.classList[0] == "complete_btn")
         list_item.parentElement.classList.toggle("completed");
+}
+
+function saveLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null)
+        todos = [];
+    else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+function getTodos() {
+    let todos;
+    if (localStorage.getItem("todos") === null)
+        todos = [];
+    else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    todos.forEach(element => {
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("div");
+
+        const item = document.createElement("li");
+        item.classList.add("item");
+        item.innerText = element;
+
+        const complete_btn = document.createElement("button");
+        complete_btn.classList.add("complete_btn");
+        complete_btn.innerHTML = '<i class="fa fa-check"></i>';
+
+        const del_btn = document.createElement("button");
+        del_btn.classList.add("del_btn");
+        del_btn.innerHTML = '<i class="fa fa-trash"></i>';
+
+        if (element != "") {
+            todoDiv.appendChild(item)
+            todoDiv.appendChild(complete_btn);
+            todoDiv.appendChild(del_btn);
+            list_items.appendChild(todoDiv);
+        }
+    });
+}
+
+function removeTodo(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null)
+        todos = [];
+    else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    if (todos.indexOf(todo) != -1) {
+        todos.splice(todos.indexOf(todo), 1)
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }
 }
